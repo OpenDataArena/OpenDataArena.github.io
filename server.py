75d40ebb13aa30d630797cd2a9ce2bee6eb649d2
Briefmind-Daily <3044153767@qq.com>
2025-06-11 11:59:25 +0800

    Update website content 2025-06-11 11:59:25

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 import webbrowser
 from pathlib import Path
 
-PORT = 8034
+PORT = 8035
 DIRECTORY = Path(__file__).parent
 
 class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
