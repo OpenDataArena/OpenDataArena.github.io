da9b4937ca508d46e3491fafa98addc78f8ff949
sunmengyuan <1192056830@qq.com>
2025-07-26 23:07:37 +0800

    Update website content 2025-07-26 23:07:37

--- js/app.js
+++ js/app.js
@@ -1481,7 +1481,6 @@ const app = createApp({
 
         // 挂载时加载数据
         onMounted(() => {
-            setCurrentPage() // 设置当前页面
             loadData()
             
             // 添加全局点击事件来关闭下拉框
